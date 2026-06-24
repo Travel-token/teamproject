d091c72aa3b325314085dc85b5a6b50d5dc06273
pcmin99 <parkchanmin990804@naver.com>
2026-06-24 23:54:56 +0900

    restart

--- app/index.js
+++
@@ -1,8 +0,0 @@
-import { Redirect } from 'expo-router';
-import { useAuth } from '../src/context/AuthContext';
-
-export default function Index() {
-  const { isLoggedIn } = useAuth();
-  return <Redirect href="/(tabs)/home" />;
-  //return <Redirect href={isLoggedIn ? '/(tabs)/home' : '/(auth)/login'} />;
-}
